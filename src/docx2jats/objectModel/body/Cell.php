e154466c1d12499414b355f6afb1072a0db8cfee
Antonio Soriano Hernández <antonio.soriano@ticarum.es>
2022-12-07 14:03:56 +0100

    Clean uneeded var

--- src/docx2jats/objectModel/body/Cell.php
+++ src/docx2jats/objectModel/body/Cell.php
@@ -45,7 +45,6 @@ class Cell extends DataObject {
 		// Prune colspan when there is a dummycell between column start and colspan
 		$this->prunedColspan = $this->colspan;
 		if ($this->colspan > 1) {
-			$cellends = $this->cellNumber + $this->colspan - 1;
 			$dummyCells = $this->getParent()->getParent()->getDummyCells();
 			foreach ($dummyCells as $pos) {
 				if ($this->cellNumber <= $pos && $this->cellNumber + $this->colspan - 1 >= $pos) {
